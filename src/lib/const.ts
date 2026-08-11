d43f7634885e8973badf9e9ae3ccfad534d0b6a7
j-Riv <jose.alfredo.rivera@gmail.com>
2026-08-11 16:09:43 -0700

    chore: Reset const

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -4,10 +4,10 @@ dotenv.config();
 
 export const USE_SINGLE_STORE = false; // set to true to only create codes on Store A
 
-export const TOTAL_CODES = 10; // 10_000
-export const CODE_PREFIX = 'SUAVE15-TEST'; // adjust per campaign
+export const TOTAL_CODES = 5_000; // 10_000
+export const CODE_PREFIX = 'SUAVE15'; // adjust per campaign
 export const BATCH_SIZE = 100;
 export const API_VERSION = '2026-07';
 
 export const LEDGER_PATH = './output/codes-ledger.json';
-export const TOP_UP_COUNT = 5; // number of codes to add in top-up script
+export const TOP_UP_COUNT = 5_000; // number of codes to add in top-up script
